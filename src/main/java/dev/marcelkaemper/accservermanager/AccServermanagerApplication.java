dbaba90140f77c9263f59d3191a7a8f16c331276
Marcel Kämper <marcelkaemper862@gmail.com>
2020-10-03 12:47:53 +0200

    Created spring boot project

---
+++ src/main/java/dev/marcelkaemper/accservermanager/AccServermanagerApplication.java
@@ -0,0 +1,13 @@
+package dev.marcelkaemper.accservermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccServermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccServermanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcelkaemper/accservermanager/AccServermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcelkaemper.accservermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccServermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
